ead8ef0b27499bf7f2b7eb467d4501177df2389d
hugo <hugo.ahmed@enti.cat>
2022-11-12 04:16:09 +0100

    Bug collisiones arreglado
    Monedas hechas, falta cappy

--- platafromas3D/Assets/Scripts/Player/P_collision.cs
+++ platafromas3D/Assets/Scripts/Player/P_collision.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class P_collision : MonoBehaviour
 {
+    public float coin_count = 8;
+
+    private void Update()
+    {
+        if (coin_count == 0)
+        {
+            SceneManager.LoadScene("Victory");
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "enemy")
         {
             Destroy(gameObject);
+            SceneManager.LoadScene("SampleScene");
+        }
+        if (collision.collider.tag == "coin")
+        {
+            coin_count -= 1;
+            
         }
     }
 }
